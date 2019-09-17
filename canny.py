4316c6e8c9e545497edddf79250b61b3a5fe663a
Yasuyuki ENDO <eyasuyuki@gmail.com>
2019-09-17 10:47:47 +0900

    more blurr, more contrast

--- canny.py
+++ canny.py
@@ -25,12 +25,36 @@ image = cv2.imread("example.jpg")
 image = imutils.resize(image, height=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-blurred = cv2.GaussianBlur(gray, (5, 5), 10, 10)
+# blur
+
+blurred = cv2.GaussianBlur(gray, (9, 9), 10, 10)
 cv2.imwrite("blurred.jpg", blurred)
+
+# lut
+
+# min_table = 45
+# max_table = 210
+min_table = 60
+max_table = 195
+diff_table = max_table - min_table
+lookup_table = np.arange(256, dtype="uint8")
+
+for i in range(0, len(lookup_table)):
+        if (i < min_table):
+                lookup_table[i] = 0
+        elif (i >= min_table and i <= max_table):
+                n = 255 * (i - min_table) / diff_table
+                #print(f"{i}, {n}")
+                lookup_table[i] = n
+        elif (i > max_table):
+                lookup_table[i] = 255
+
+contrast = cv2.LUT(blurred.copy(), lookup_table)
+cv2.imwrite("contrast.jpg", contrast)
 
 # canny
 
-edged = cv2.Canny(blurred, 20, 60, 255)
+edged = cv2.Canny(contrast, 20, 80, 255)
 cv2.imwrite("edged.jpg", edged)
 
 # find contors
@@ -38,12 +62,12 @@ cv2.imwrite("edged.jpg", edged)
 cnts0 = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
 # filter noize
-thCnts = []
+""" thCnts = []
 for c in cnts0:
         (x, y, z, w) = cv2.boundingRect(c)
         a = cv2.contourArea(c)
-        print(f"{x}, {y}, {z}, {w}, {a}")
-        if (a >= 20):
+#        print(f"{x}, {y}, {z}, {w}, {a}")
+        if (a >= 10):
                 thCnts.append(c)
 
 background = np.zeros_like(image, np.uint8)
@@ -51,10 +75,10 @@ background = np.zeros_like(image, np.uint8)
 edged_cnts = cv2.drawContours(background, thCnts, -1, (255, 255, 255), 1)
 edged_cnts = cv2.cvtColor(edged_cnts, cv2.COLOR_BGR2GRAY);
 cv2.imwrite("edged_cnts.jpg", edged_cnts)
-
+ """
 # https://github.com/DevashishPrasad/LCD-OCR/blob/master/code.py
 
-dilate = cv2.dilate(edged_cnts, None, iterations=16)
+dilate = cv2.dilate(edged, None, iterations=16)
 cv2.imwrite("dilate.jpg", dilate)
 
 erode = cv2.erode(dilate, None, iterations=16)
@@ -79,7 +103,7 @@ cv2.imwrite("contoured.jpg", contoured)
 orig = image.copy()
 for c in cnts:
         (x, y, w, h) = cv2.boundingRect(c)
-        print(f"{x}, {y}, {w}, {h}")
+        #print(f"{x}, {y}, {w}, {h}")
         if cv2.contourArea(c) < 900:
                 cv2.drawContours(mask2, [c], -1, 0, -1)
                 continue
