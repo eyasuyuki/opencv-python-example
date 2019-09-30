d76ef477f85c8be650b727eb5a05d8b59fbd1486
Yasuyuki ENDO <eyasuyuki@gmail.com>
2019-09-30 21:56:46 +0900

    split word group

--- canny.py
+++ canny.py
@@ -1,4 +1,5 @@
 import datetime
+import itertools
 import math
 
 from imutils import contours
@@ -19,75 +20,83 @@ DIGITS_LOOKUP = DIGITS_LOOKUP = {
     (1, 1, 1, 1, 0, 1, 1): 9
 }
 
+
 def gray(img):
-        grayed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("grayed.jpg", grayed)
-        return grayed
+    grayed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("grayed.jpg", grayed)
+    return grayed
+
 
 def denoise(img):
-        denoised = cv2.fastNlMeansDenoising(img, 10, 10, 7, 21)
-        cv2.imwrite("denoised.jpg", denoised)
-        return denoised
+    denoised = cv2.fastNlMeansDenoising(img, 10, 10, 7, 21)
+    cv2.imwrite("denoised.jpg", denoised)
+    return denoised
+
 
 def lut(img, min_table, max_table):
-        diff_table = max_table - min_table
-        lookup_table = np.arange(256, dtype="uint8")
+    diff_table = max_table - min_table
+    lookup_table = np.arange(256, dtype="uint8")
 
-        for i in range(0, len(lookup_table)):
-                if (i < min_table):
-                        lookup_table[i] = 0
-                elif (i >= min_table and i <= max_table):
-                        n = 255 * (i - min_table) / diff_table
-                        #print(f"{i}, {n}")
-                        lookup_table[i] = n
-                elif (i > max_table):
-                        lookup_table[i] = 255
+    for i in range(0, len(lookup_table)):
+        if (i < min_table):
+            lookup_table[i] = 0
+        elif (i >= min_table and i <= max_table):
+            n = 255 * (i - min_table) / diff_table
+            # print(f"{i}, {n}")
+            lookup_table[i] = n
+        elif (i > max_table):
+            lookup_table[i] = 255
 
-        contrast = cv2.LUT(img.copy(), lookup_table)
-        cv2.imwrite("contrast.jpg", contrast)
-        return contrast
+    contrast = cv2.LUT(img.copy(), lookup_table)
+    cv2.imwrite("contrast.jpg", contrast)
+    return contrast
+
 
 def canny(img):
-        edged = cv2.Canny(img, 20, 80, 255)
-        cv2.imwrite("edged.jpg", edged)
-        return edged
+    edged = cv2.Canny(img, 20, 80, 255)
+    cv2.imwrite("edged.jpg", edged)
+    return edged
+
 
 def filter_noise(img):
-        # find contors
+    # find contors
 
-        cnts0 = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+    cnts0 = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-        # filter noize
+    # filter noize
 
-        thCnts = []
-        for c in cnts0:
-                (x, y, z, w) = cv2.boundingRect(c)
-                a = cv2.contourArea(c)
-                # print(f"{x}, {y}, {z}, {w}, {a}")
-                if (a >= 10):
-                        thCnts.append(c)
+    thCnts = []
+    for c in cnts0:
+        (x, y, z, w) = cv2.boundingRect(c)
+        a = cv2.contourArea(c)
+        # print(f"{x}, {y}, {z}, {w}, {a}")
+        if (a >= 10):
+            thCnts.append(c)
 
-        background = np.zeros_like(img, np.uint8)
+    background = np.zeros_like(img, np.uint8)
 
-        edged_cnts = cv2.drawContours(background, thCnts, -1, (255, 255, 255), 1)
-        cv2.imwrite("edged_cnts.jpg", edged_cnts)
-        return edged_cnts
+    edged_cnts = cv2.drawContours(background, thCnts, -1, (255, 255, 255), 1)
+    cv2.imwrite("edged_cnts.jpg", edged_cnts)
+    return edged_cnts
+
 
 def closing(img):
-        # https://github.com/DevashishPrasad/LCD-OCR/blob/master/code.py
+    # https://github.com/DevashishPrasad/LCD-OCR/blob/master/code.py
 
-        dilate = cv2.dilate(edged, None, iterations=4)
-        cv2.imwrite("dilate.jpg", dilate)
+    dilate = cv2.dilate(edged, None, iterations=4)
+    cv2.imwrite("dilate.jpg", dilate)
 
-        erode = cv2.erode(dilate, None, iterations=4)
-        cv2.imwrite("erode.jpg", erode)
+    erode = cv2.erode(dilate, None, iterations=4)
+    cv2.imwrite("erode.jpg", erode)
 
-        return dilate, erode
+    return dilate, erode
+
 
 def adaptive_threshold(img):
-        ret, th = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        cv2.imwrite(datetime.utcnow().strftime('th-%Y%m%d%H%M%S_%f.jpg'), th)
-        # TODO recognize
+    ret, th = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    cv2.imwrite(datetime.utcnow().strftime('th-%Y%m%d%H%M%S_%f.jpg'), th)
+    # TODO recognize
+
 
 # load image
 image = cv2.imread("example.jpg")
@@ -129,14 +138,41 @@ cnts = cv2.findContours(erode.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
 
 sorted_cnts = contours.sort_contours(cnts, method="left-to-right")[0]
 
-textbox = image.copy()
-(fx, fy, fw, fh) = cv2.boundingRect(sorted_cnts[0])
-for c in sorted_cnts:
-        (x, y, w, h) = cv2.boundingRect(c)
-        rad = math.atan2(x - fx, y - fy)
-        roi = contrast[y:y + h, x:x + w] # clip numeric segment
-        adaptive_threshold(roi) # extract text segment
-        print(f"{x}, {y}, {w}, {h}, {rad}")
-        textbox = cv2.rectangle(textbox, (x, y), (x + w, y + h), (0, 255, 0), 2)
+pairs = itertools.combinations(sorted_cnts, 2)
 
-cv2.imwrite("textbox.jpg", textbox)
+# word group
+words = [[]]
+for p in pairs:
+    p0 = p[0]
+    p1 = p[1]
+    (x0, y0, w0, h0) = cv2.boundingRect(p0)
+    (x1, y1, w1, h1) = cv2.boundingRect(p1)
+    d = np.linalg.norm(np.array([x1, y1]) - np.array([x0, y0]))
+    if d < h0:
+        print(f"x0={x0}, y0={y0}, x1={x1}, y1={y1}, d={d}, h0={h0}")
+        flat = itertools.chain.from_iterable(words)
+        if len(words) <= 0:
+            words.append(list([p0, p1]))
+        elif p0 not in flat and p1 not in flat:
+            words.append(list([p0, p1]))
+        else:
+            for w in words:
+                if p0 in w and p1 not in w:
+                    w = w.append(p1)
+                elif p1 in w and p0 not in w:
+                    w = w.append(p0)
+for i, w in enumerate(words):
+    for c in w:
+        (x, y, w, h) = cv2.boundingRect(c)
+        print(f"i={i}, x={x}, y={y}, w={w}, h={h}")
+# textbox = image.copy()
+# (fx, fy, fw, fh) = cv2.boundingRect(sorted_cnts[0])
+# for c in sorted_cnts:
+#     (x, y, w, h) = cv2.boundingRect(c)
+#     rad = math.atan2(x - fx, y - fy)
+#     roi = contrast[y:y + h, x:x + w]  # clip numeric segment
+#     adaptive_threshold(roi)  # extract text segment
+#     print(f"{x}, {y}, {w}, {h}, {rad}")
+#     textbox = cv2.rectangle(textbox, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#
+# cv2.imwrite("textbox.jpg", textbox)
